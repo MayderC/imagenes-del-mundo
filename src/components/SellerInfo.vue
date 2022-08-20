fcb2802ec7f8ea67e055131e5b40933953442277
Mayder C <maydercortes1@gmail.com>
2022-08-19 21:33:16 -0600

    init style desktop, sellerList

--- src/components/SellerInfo.vue
+++ src/components/SellerInfo.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="bg-zinc-500 rounded-md">
+  <div
+    class="bg-zinc-500 bg-gradient-to-br from-blue-500 to-pink-500 rounded-md p-4"
+  >
     <p class="text-left">Nombre: {{ seller.name }}</p>
-    <p class="text-left">Puntos: {{ seller.points }}</p>
-    <p class="text-left">pt restantes: {{ limit - seller.points }}</p>
+    <p class="text-left">Puntos</p>
+    <p class="text-left">Actuales: {{ seller.points }}</p>
+    <p class="text-left">Restantes: {{ limit - seller.points }}</p>
   </div>
 </template>
 
--- src/components/SellerList.vue
+++ src/components/SellerList.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-    class="bg-zinc-700 w-80 max-h-[85vh] absolute flex flex-col top-0 rounded-md p-4"
+    class="bg-zinc-800 w-80 max-h-[85vh] gap-4 absolute flex flex-col top-0 rounded-md p-4"
   >
+    <h2 class="text-purple-400 font-bold">Puntos de los vendedores</h2>
     <seller-info
       v-for="seller in sellers"
       :seller="seller"
