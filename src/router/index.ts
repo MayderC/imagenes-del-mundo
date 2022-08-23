e835e53ae1bd8ab81e321062d5e02f0ec27f2283
Mayder C <maydercortes1@gmail.com>
2022-08-23 04:46:54 -0600

    add hash mode route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,9 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+  RouteRecordRaw,
+} from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import store from "@/store";
 import NotFound from "@/views/NotFound.vue";
@@ -21,7 +26,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
