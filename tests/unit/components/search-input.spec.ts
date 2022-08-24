be2977bebda2554b85222452827b851f35c1c933
Mayder C <maydercortes1@gmail.com>
2022-08-23 23:53:11 -0600

    global wrapper in search-input

--- tests/unit/components/search-input.spec.ts
+++ tests/unit/components/search-input.spec.ts
@@ -2,16 +2,16 @@ import SearchInput from "@/components/SearchInput.vue";
 import { mount } from "@vue/test-utils";
 
 describe("Test SearchInput component", () => {
+  const wrapper = mount(SearchInput);
+  const value = "Hola";
+
   it("checks if the text inserted in the input is saved in the 'toSearch' property", async () => {
     const value = "hello input";
-    const wrapper = mount(SearchInput);
     await wrapper.find("input[id='search']").setValue(value);
     expect(wrapper.vm["toSearch"]).toBe(value);
   });
 
-  it("checks if event search was emmited", async () => {
-    const wrapper = mount(SearchInput);
-    const value = "Hola";
+  it("checks if event search was emitted", async () => {
     await wrapper.find("input[id='search']").setValue(value);
     wrapper.find("form").trigger("submit");
 
